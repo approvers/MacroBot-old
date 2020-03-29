843fc980d3e2bde9c1e1765b74de8f7d33bd2323
isso0424 <isso04240129@gmail.com>
2020-03-30 04:49:46 +0900

    [Fix]Loopの処理をClientで発火するように変更

--- main.py
+++ main.py
@@ -2,14 +2,5 @@ from discord.ext import tasks
 
 from MacroBot.Client import Client
 
-
 client = Client()
-
-
-@tasks.loop(seconds=60)
-async def loop():
-    await client.macro_loop()
-
-
-loop.start()
 client.run()
